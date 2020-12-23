a7a17e27d67c948ad825bab15cfbe56aa6364bc7
Jeremiah Koeiman <9001603@student.zadkine.nl>
2020-12-23 13:35:27 +0100

    Video #17 - Custom Fonts

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as Font from 'expo-font'
+
 import Home from './screens/Home'
 
 export default function App() {
+
+  useEffect(() => {
+    Font.loadAsync({
+      'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+      'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+    })
+  }, [])
+
+
   return (
     <Home/>
   );
