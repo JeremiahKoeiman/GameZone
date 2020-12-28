d94e722c0ba0fe5865bf2c7848aee6785bcd2d75
Jeremiah Koeiman <9001603@student.zadkine.nl>
2020-12-28 14:48:39 +0100

    Video #20 - Stack Navigator

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Home from './screens/Home'
+import Navigator from './routes/homeStack'
 
 
 export default function App() {
 
     return (
-      <Home/>
+      <Navigator/>
     );
 }
 
