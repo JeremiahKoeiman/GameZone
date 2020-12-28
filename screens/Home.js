5f103bca9403ae532c24ec51adb046431b1724ec
Jeremiah Koeiman <9001603@student.zadkine.nl>
2020-12-28 14:58:31 +0100

    Video #21 - Navigating Around

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { View, Text, TextBase } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 import { globalStyles } from '../styles/Global'
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails')
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home screen</Text>
+            <Button title='Go to review details' onPress={pressHandler}/>
         </View>
     )
 }
